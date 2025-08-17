a487eca1fdc712057f0c1299de8fb4021beaad3e
Sina-TehraniFard <tf.sin.system@gmail.com>
2025-08-18 00:53:08 +0900

    初期化

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-  content: ['./src/**/*.{ts,tsx,js,jsx,mdx,css}'],
-  theme: { 
-    extend: {} 
-  },
-  plugins: [],
-} satisfies Config
